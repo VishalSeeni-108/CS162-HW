3e9c050a61e84dd80a8cb68b6d815042fbbbc0b1
Viz108 <67886538+Viz108@users.noreply.github.com>
2025-01-28 10:42:11 -0800

    Finished up to problem 1.5

--- HW 2/fredfeng CS162 master homework-hw2/lib/part1/part1.ml
+++ HW 2/fredfeng CS162 master homework-hw2/lib/part1/part1.ml
@@ -6,8 +6,13 @@ let todo () = failwith "TODO"
 let singletons (xs : 'a list) : 'a list list = 
   List.map (fun x -> [x]) xs;;
 
-let map2d (f : 'a -> 'b) (xss : 'a list list) : 'b list list = todo ()
-let product (xs : 'a list) (ys : 'b list) : ('a * 'b) list list = todo ()
+let map2d (f : 'a -> 'b) (xss : 'a list list) : 'b list list = 
+  List.map (fun x -> List.map f x) xss;;
+
+let product (xs : 'a list) (ys : 'b list) : ('a * 'b) list list = 
+  List.map (fun x -> List.map (fun y -> (x, y)) ys) xs;;
+
+
 let power (xs : 'a list) : 'a list list = todo ()
 
 let both : 'a option -> 'b option -> ('a * 'b) option =
